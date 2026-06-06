b28fad0b9c3260ad63216224ab8c53aa4553ac1c
ManuelSfilho <52848138+ManuelSfilho@users.noreply.github.com>
2026-06-06 17:39:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,7 @@
-const CACHE = "v1";
-
-self.addEventListener("install", event => {
+self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
